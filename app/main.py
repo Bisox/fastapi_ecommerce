dd2ff22126c60a60e7c61feaa291a681b5483d89
Evgen Bratenkov <giertrut@mail.ru>
2024-08-13 18:44:47 +0300

    added schemas

--- app/main.py
+++ app/main.py
@@ -1,4 +1,6 @@
 from fastapi import FastAPI
+from app.routers import category, products
+
 
 app = FastAPI()
 
@@ -11,3 +13,7 @@ async def root():
 @app.get("/hello/{name}")
 async def say_hello(name: str):
     return {"message": f"Hello {name}"}
+
+
+app.include_router(category.router)
+app.include_router(products.router)
