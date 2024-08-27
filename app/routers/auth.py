16bbd7a6feea96c70be2400dced51f2047672634
Evgen Bratenkov <giertrut@mail.ru>
2024-08-27 19:01:46 +0300

    update routers/auth

--- app/routers/auth.py
+++ app/routers/auth.py
@@ -9,7 +9,7 @@ from typing import Annotated
 from passlib.context import CryptContext
 
 from datetime import datetime, timedelta
-from jose import jwt
+from jose import jwt, JWTError
 
 from app.models.user import User
 from app.schemas import CreateUser
@@ -83,6 +83,45 @@ async def login(db: Annotated[AsyncSession, Depends(get_db)], form_data: Annotat
     }
 
 
+async def get_current_user(token: Annotated[str, Depends(oauth2_scheme)]):
+    try:
+        payload = jwt.decode(token, settings.SECRET_KEY, algorithms=[settings.ALGORITHM])
+        username: str = payload.get('sub')
+        user_id: int = payload.get('id')
+        is_admin: str = payload.get('is_admin')
+        is_supplier: str = payload.get('is_supplier')
+        is_customer: str = payload.get('is_customer')
+        expire = payload.get('exp')
+        if username is None or user_id is None:
+            raise HTTPException(
+                status_code=status.HTTP_401_UNAUTHORIZED,
+                detail='Could not validate user'
+            )
+        if expire is None:
+            raise HTTPException(
+                status_code=status.HTTP_400_BAD_REQUEST,
+                detail="No access token supplied"
+            )
+        if datetime.now() > datetime.fromtimestamp(expire):
+            raise HTTPException(
+                status_code=status.HTTP_403_FORBIDDEN,
+                detail="Token expired!"
+            )
+
+        return {
+            'username': username,
+            'id': user_id,
+            'is_admin': is_admin,
+            'is_supplier': is_supplier,
+            'is_customer': is_customer,
+        }
+    except JWTError:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail='Could not validate user'
+        )
+
+
 @router.get('/read_current_user')
-async def read_current_user(user: User = Depends(oauth2_scheme)):
-    return user
+async def read_current_user(user: dict = Depends(get_current_user)):
+    return {'User': user}
