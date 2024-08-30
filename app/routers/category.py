f55e246f77e2490faaa19d3ad8266a2a6962fef3
Evgen Bratenkov <giertrut@mail.ru>
2024-08-30 18:38:22 +0300

    update routers/category update endpoints

--- app/routers/category.py
+++ app/routers/category.py
@@ -8,6 +8,7 @@ from sqlalchemy.ext.asyncio import AsyncSession
 from app.backend.db_depends import get_db
 from app.models import *
 from app.schemas import CreateCategory
+from app.services.auth_helpers import get_current_user
 
 router = APIRouter(prefix='/category', tags=['category'])
 
@@ -21,58 +22,83 @@ async def get_all_categories(db: Annotated[AsyncSession, Depends(get_db)]):
 
 
 @router.post('/create')
-async def create_category(db: Annotated[AsyncSession, Depends(get_db)], create_category: CreateCategory):
-    category_create = (insert(Category).values(name=create_category.name,
-                                               parent_id=create_category.parent_id,
-                                               slug=slugify(create_category.name)))
+async def create_category(db: Annotated[AsyncSession, Depends(get_db)],
+                          create_category: CreateCategory,
+                          get_user: Annotated[dict, Depends(get_current_user)]):
+    if get_user.get('is_admin'):
+        category_create_query = (insert(Category).values(name=create_category.name,
+                                                   parent_id=create_category.parent_id,
+                                                   slug=slugify(create_category.name)))
 
-    await db.execute(category_create)
-    await db.commit()
-    return {
-        'status_code': status.HTTP_201_CREATED,
-        'transaction': 'Successful'
-    }
+        await db.execute(category_create_query)
+        await db.commit()
+        return {
+            'status_code': status.HTTP_201_CREATED,
+            'transaction': 'Successful'
+        }
+    else:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail='You must be admin user for this'
+        )
 
 
 @router.put('/update_category')
-async def update_category(db: Annotated[AsyncSession, Depends(get_db)], category_id: int, update_category: CreateCategory):
-    category_query = select(Category).where(Category.id == category_id)
-    category = await db.scalar(category_query)
+async def update_category(db: Annotated[AsyncSession, Depends(get_db)],
+                          category_id: int,
+                          update_category: CreateCategory,
+                          get_user: Annotated[dict, Depends(get_current_user)]):
+    if get_user.get('is_admin'):
+        category_query = select(Category).where(Category.id == category_id)
+        category = await db.scalar(category_query)
 
-    if category is None:
+        if category is None:
+            raise HTTPException(
+                status_code=status.HTTP_404_NOT_FOUND,
+                detail='There is no category found'
+            )
+        update_category_query = update(Category).where(Category.id == category_id).values(
+                                                        name=update_category.name,
+                                                        slug=slugify(update_category.name),
+                                                        parent_id=update_category.parent_id)
+        await db.execute(update_category_query)
+
+        await db.commit()
+        return {
+            'status_code': status.HTTP_200_OK,
+            'transaction': 'Category update is successful'
+        }
+    else:
         raise HTTPException(
-            status_code=status.HTTP_404_NOT_FOUND,
-            detail='There is no category found'
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail='You must be admin user for this'
         )
-    update_category_query = update(Category).where(Category.id == category_id).values(
-                                                    name=update_category.name,
-                                                    slug=slugify(update_category.name),
-                                                    parent_id=update_category.parent_id)
-    await db.execute(update_category_query)
 
-    await db.commit()
-    return {
-        'status_code': status.HTTP_200_OK,
-        'transaction': 'Category update is successful'
-    }
 
 
 @router.delete('/delete')
-async def delete_category(db: Annotated[AsyncSession, Depends(get_db)], category_id: int):
+async def delete_category(db: Annotated[AsyncSession, Depends(get_db)],
+                          category_id: int,
+                          get_user: Annotated[dict, Depends(get_current_user)]):
+    if get_user.get('is_admin'):
+        category_query = select(Category).where(Category.id == category_id)
+        category = db.scalar(category_query)
 
-    category_query = select(Category).where(Category.id == category_id)
-    category = db.scalar(category_query)
+        if category is None:
+            raise HTTPException(
+                status_code=status.HTTP_404_NOT_FOUND,
+                detail='There is no category found'
+            )
+        update_query = update(Category).where(Category.id == category_id).values(is_active=False)
 
-    if category is None:
+        await db.execute(update_query)
+        await db.commit()
+        return {
+            'status_code': status.HTTP_200_OK,
+            'transaction': 'Category delete is successful'
+        }
+    else:
         raise HTTPException(
-            status_code=status.HTTP_404_NOT_FOUND,
-            detail='There is no category found'
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail='You must be admin user for this'
         )
-    update_query = update(Category).where(Category.id == category_id).values(is_active=False)
-
-    await db.execute(update_query)
-    await db.commit()
-    return {
-        'status_code': status.HTTP_200_OK,
-        'transaction': 'Category delete is successful'
-    }
